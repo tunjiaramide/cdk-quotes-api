44b39ad4a36ae5085bf830e42f5de74dacdd5ed2
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-07-26 17:10:04 +0100

    full rest with dynamodb

--- lib/cdk-quotes-api-stack.ts
+++ lib/cdk-quotes-api-stack.ts
@@ -3,21 +3,45 @@ import { Construct } from 'constructs';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 
 export class CdkQuotesApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const getQuotes = new Function(this, "GetQuoteLambda", {
+    const table = new Table(this, "quotesTbl", {
+        partitionKey: { name: 'id', type: AttributeType.STRING},
+        billingMode: BillingMode.PAY_PER_REQUEST,
+        removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    const handlerFunction = new Function(this, "quotesHandler", {
       runtime: Runtime.NODEJS_22_X,
       code: Code.fromAsset("lambdas"),
-      handler: 'getQuotes.handler',
+      handler: 'app.handler',
+      environment: {
+        MY_TABLE: table.tableName
+      }
     })
+
+    table.grantReadWriteData(handlerFunction)
 
     const api = new RestApi(this, 'quotes-api', {
         description: 'Quotes API',
     })
 
+    const handlerIntegration = new apigateway.LambdaIntegration(handlerFunction)
+
+    const mainPath = api.root.addResource("quotes");
+    const idPath = mainPath.addResource("{id}");
+
+    mainPath.addMethod("GET", handlerIntegration);
+    mainPath.addMethod("POST", handlerIntegration);
+    idPath.addMethod("DELETE", handlerIntegration);
+    idPath.addMethod("GET", handlerIntegration);
+    idPath.addMethod("PUT", handlerIntegration);
+  
   }
 }
